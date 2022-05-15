28ca4a394a197b5af54e485c269e376b567f75ad
Everton Araujo <everton.oa@gmail.com>
2022-05-15 12:02:53 +0100

    Add open account test

--- src/test/java/testcases/OpenAccountTest.java
+++ src/test/java/testcases/OpenAccountTest.java
@@ -1,5 +1,9 @@
 package testcases;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import base.TestBase;
@@ -8,10 +12,15 @@ import utilities.TestUtil;
 public class OpenAccountTest extends TestBase {
 	
 	@Test (dataProviderClass = TestUtil.class, dataProvider = "dp")
-	public void openAccountTest (String customer, String currency) {
+	public void openAccountTest (String customer, String currency, String runMode) {
 		
+		click("openAccountBtn_CSS");
+		select("customerNameDd_ID", customer);
+		select("currencyDd_ID", currency);
+		click("processBtn_CSS");
 		
-		
+		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+		alert.accept();
 	}
 
 }
